91e78819c55cd8f2ba65ebad9fe9164b855e62ef
sebwebneb <sebwebneb@proton.me>
2025-12-27 03:11:27 +0000

    Update entry.cpp

--- entry.cpp
+++ entry.cpp
@@ -1,13 +1,4 @@
-#include "includes.h"
-#include "detections/dpc.h"
-#include "integrity/hvci.h"
-#include "integrity/usermode threads.h"
-#include "integrity/vulnerable_drivers.h"
-#include "detections/dma.h"
-#include "detections/kdmapper.h"
-#include "integrity/integrity.h"
-#include "detections/mouse.h"
-#include "detections/efi.h"
+#include "dpc.h"
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
     UNREFERENCED_PARAMETER(RegistryPath);
